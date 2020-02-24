023d07560fcc7bdb82a357897c8cf0b7ddeea3e8
stenmr <stenmr@tlu.ee>
2020-02-24 18:57:13 +0200

    Remove animation, add options

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ const dateFormatter = new Intl.DateTimeFormat("et", {
     year: "numeric"
 });
 
-// Kasutame requestAnimationFrame optimaalseks animatsiooni esituseks
-function clock() {
+function date() {
 
+    const dateElement = document.getElementById("date");
     const date = Date.now();
+    // Mugavalt vormistab kuupäeva
+    const formattedDate = dateFormatter.format(date);
+
+    dateElement.innerHTML = "Täna on " + formattedDate;
 
     const formattedTime = timeFormatter.format(date);
 
-    // Count-i kasutame canvaste kättesaamiseks nende id järgi (mis on samuti numbrid)
-    // Võib-olla saab lihtsamalt kui kasutada document.getElementsByTagName("canvas") kuid ei ole kindel kas see garanteerib sama järjekorra alati
     let count = 1;
     for (const char of formattedTime) {
 
@@ -30,52 +32,12 @@ function clock() {
         // Kasutame ühte SVG faili, kus on kõik numbrid ja koolon olemas SVG fragmentidena
         canvas.style.mask = `url(combined.svg#${char}) 50% 50% no-repeat`;
         canvas.style.webkitMask = `url(combined.svg#${char}) 50% 50% no-repeat`;
-
-        const ctx = canvas.getContext("2d");
-
-        // Genereerime tühja imageData
-        const imageData = ctx.createImageData(canvas.width, canvas.height);
-
-        // Loop peab hüppama nelja võrra
-        // Sest me muudame 4 väärtust korraga pikslis igal iteratsioonil (RGBA)
-        for (let i = 0; i < imageData.data.length; i += 4) {
-
-            // Võtame aja dokumendi loomise algusest millisekundites
-            // On väga hea muutuja, sest iga kord kui kutsume selle funktsiooni esile, on see arv suurem
-            const t = window.performance.now();
-
-            // Kasutame sin ja cos kuna see on hea viis arvu normaliseerimiseks
-            // jagamine 1536-ga on lihtsalt meelepärane värvi muutumiskiirus
-            const r = 64 + 64 * Math.sin(t / 1536);
-            const g = 92 + 64 * Math.cos(t / 1536);
-
-            // Paneme uued värvid käesolevasse pikslisse, sinine ja alpha jätame konstantseks
-            [imageData.data[i + 0], imageData.data[i + 1], imageData.data[i + 2], imageData.data[i + 3]] = [r, g, 166, 255];
-        }
-
-        // Kirjutame üle uue imageData-ga
-        ctx.putImageData(imageData, 0, 0);
-
     }
-    requestAnimationFrame(clock);
-};
-
-clock()
-
-function date() {
-
-    const dateElement = document.getElementById("date");
-    const date = Date.now();
-    // Mugavalt vormistab kuupäeva
-    const formattedDate = dateFormatter.format(date);
-
-    dateElement.innerHTML = "Täna on " + formattedDate;
 }
 
 // See jookseb async esimesel jooksutamisel
 // kuid ma ei ole kindel kas setInterval on async
-// igatahes korra minutis peaks piisama, sest kella animatsioon kasutab niigi ressursse
 setTimeout(() => {
     date();
-    setInterval(date, 60 * 1000);
+    setInterval(date, 1000);
 });
